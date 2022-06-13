9fc2e7c5e51bb7a5b59a9bc7ef787e306169235d
Devin Spivey <56698580+dss0029@users.noreply.github.com>
2022-06-13 18:06:27 -0500

    Create LuaAutopilot.lua

---
+++ Autopilot/LuaAutopilot.lua
@@ -0,0 +1,16 @@
+-- warn the user if wind speed exceeds a threshold, failsafe if a second threshold is exceeded
+
+-- note that this script is only intended to be run on ArduPlane
+local count = 0
+
+function update()
+   gcs:send_test(0, count)
+	 if count > 100 then
+	       gcs:send_text(0, "Time to go home.")
+	       vehicle:set_mode(11) -- FIXME: should be an enum.  11 is RTL.
+	       return
+   end
+   count = count + 1
+end
+
+return update, 1000
