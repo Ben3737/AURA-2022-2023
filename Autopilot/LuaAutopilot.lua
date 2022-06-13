92305a1ceac582b704b907ea64a5c0a94751a8b1
Devin Spivey <56698580+dss0029@users.noreply.github.com>
2022-06-13 18:18:56 -0500

    Update LuaAutopilot.lua

--- Autopilot/LuaAutopilot.lua
+++ Autopilot/LuaAutopilot.lua
@@ -4,13 +4,14 @@
 local count = 0
 
 function update()
-   gcs:send_test(0, count)
-	 if count > 100 then
-	       gcs:send_text(0, "Time to go home.")
-	       vehicle:set_mode(11) -- FIXME: should be an enum.  11 is RTL.
-	       return
-   end
-   count = count + 1
+	gcs:send_test(0, count)
+	if count > 100 then
+		gcs:send_text(0, "Time to go home.")
+		vehicle:set_mode(11) -- FIXME: should be an enum.  11 is RTL.
+		return
+	end
+	count = count + 1
+   	return update, 1000
 end
 
 return update, 1000
