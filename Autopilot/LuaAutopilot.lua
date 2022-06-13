534f26fc42eb1444ce4e17c3b26ab7b492cd2210
Devin Spivey <56698580+dss0029@users.noreply.github.com>
2022-06-13 18:59:45 -0500

    Update LuaAutopilot.lua

--- Autopilot/LuaAutopilot.lua
+++ Autopilot/LuaAutopilot.lua
@@ -1,8 +1,10 @@
 -- note that this script is only intended to be run on ArduPlane
 local count = 0
+local scripting_rc_1 = rc:find_channel_for_option(300)
 
 function update()
 	gcs:send_text(0, count)
+	gcs:send_text(0, rc:get_pwm(8))
 	if count > 100 then
 		gcs:send_text(0, "Time to go home.")
 		vehicle:set_mode(11) -- FIXME: should be an enum.  11 is RTL.
